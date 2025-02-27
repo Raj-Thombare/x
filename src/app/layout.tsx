58a25300006536fdb976c275a50b07a6c68826e1
Raj Thombare <rajthombare3524@gmail.com>
2025-02-27 23:33:41 +0530

    add custom signin page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import LeftBar from "@/components/LeftBar";
-import RightBar from "@/components/RightBar";
 import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
@@ -11,28 +9,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-  modal,
 }: Readonly<{
   children: React.ReactNode;
-  modal?: React.ReactNode;
 }>) {
   return (
     <ClerkProvider>
       <html lang='en'>
-        <body>
-          <div className='max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl xxl:max-w-screen-xxl mx-auto flex justify-between'>
-            <div className='px-2 xsm:px-4 xxl:px-8 '>
-              <LeftBar />
-            </div>
-            <div className='flex-1 lg:min-w-[600px] border-x-[1px] border-borderGray '>
-              {children}
-              {modal}
-            </div>
-            <div className='hidden lg:flex ml-4 md:ml-8 flex-1 '>
-              <RightBar />
-            </div>
-          </div>
-        </body>
+        <body>{children}</body>
       </html>
     </ClerkProvider>
   );
